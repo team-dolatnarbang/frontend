79e601774b9115a38bfdc2b3e56df0461f9d6716
bbzjun <wowns0912@naver.com>
2026-04-02 23:02:52 +0900

    feat: Map(Detail, Listen, End) 수정

--- dolatnarbang_fe/src/pages/user/MapDetailPage.tsx
+++ dolatnarbang_fe/src/pages/user/MapDetailPage.tsx
@@ -19,7 +19,7 @@ export default function MapDetailPage() {
         style={{
           position: 'relative',
           width: '100%',
-          height: '464px',
+          flex: 1,
           backgroundColor: '#D9D9D9',
           flexShrink: 0,
           overflow: 'hidden',
