cb2529ad8f7ed938145e996ea236926a4e889ae5
강은우 <80903685+eunwoo66@users.noreply.github.com>
2026-04-03 09:04:25 +0900

    feat: 루트 페이지

--- dolatnarbang_fe/src/pages/user/RootPage.tsx
+++ dolatnarbang_fe/src/pages/user/RootPage.tsx
@@ -5,24 +5,26 @@ export default function RootPage() {
   const navigate = useNavigate()
 
   // 2초 후 자동 이동
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      navigate('/onboarding')
-    }, 2000)
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     navigate('/onboarding')
+  //   }, 2000)
 
-    return () => clearTimeout(timer)
-  }, [navigate])
+  //   return () => clearTimeout(timer)
+  // }, [navigate])
 
   return (
     <div
       style={{
         height: '100vh',
-        background: 'var(--vapor-color-blue-200)',
+        backgroundImage: 'url(/images/mainBackground.png)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
 
         display: 'flex',
         flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
+        // alignItems: 'center',
+        // justifyContent: 'center',
       }}
     >
       {/* 로고 영역 */}
@@ -31,16 +33,25 @@ export default function RootPage() {
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
-
+          marginTop: '180px',
           transform: 'translateY(-60px)',
         }}
       >
         <img
-          src="/images/mainLogo/logo.svg"
+          src="/images/subLogo.png"
+          alt="sub logo"
+          style={{
+            width: '50px',
+            height: '50px',
+            objectFit: 'contain',
+          }}
+        />
+        <img
+          src="/images/mainLogo.svg"
           alt="logo"
           style={{
-            width: '140px',
-            height: '140px',
+            width: '240px',
+            height: '100px',
             objectFit: 'contain',
           }}
         />
